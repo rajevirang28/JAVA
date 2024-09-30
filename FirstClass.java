428c5bc3e6f5526fbf214163025aa1bc2c6f8c20
rajevirang28 <158153320+rajevirang28@users.noreply.github.com>
2024-09-30 14:10:30 +0000

    hello world

---
+++ FirstClass.java
@@ -0,0 +1,11 @@
+class FirstClass
+{
+    public static void main (String args[])
+    {
+        System.out.println("Hello World");
+    }
+}
+
+//compilation : source code.java in JDK and then byte code.java 
+
+//Execution : Byte code convernt in native code (JVM) 
