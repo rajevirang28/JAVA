4380795c639372f583f56b2bf4ae8bc92a1dd384
rajevirang28 <158153320+rajevirang28@users.noreply.github.com>
2024-11-17 06:44:08 +0000

    scanner

--- 01_basics/Input.java
+++ 01_basics/Input.java
@@ -4,12 +4,10 @@ public class Input
 {
     public static void main(String[] args) 
     {
-        try (Scanner sc = new Scanner(System.in))
-        {
+        Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int sum = a + b;
         System.out.println(sum);
-        }
     }
 }
