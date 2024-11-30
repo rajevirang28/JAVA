ce6d4921d8ea666f9fe8230a8d0971ae04d2d777
rajevirang28 <158153320+rajevirang28@users.noreply.github.com>
2024-11-30 06:56:41 +0000

    for

---
+++ 01_basics/loop.java
@@ -0,0 +1,13 @@
+public class loop {
+
+    public static void main(String[] args){
+        // counter++ => counter = counter +1
+        // for(int counter = 0; counter < 11; counter++){
+            // System.out.println(counter+" ");
+        // }
+
+        for(int i = 0; i<11; i++){
+            System.out.println(i);
+        }
+    }
+}
