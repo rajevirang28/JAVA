000e596069fd933bfb80f2e433d9b2bbce347889
rajevirang28 <158153320+rajevirang28@users.noreply.github.com>
2024-11-30 07:08:13 +0000

    do while

---
+++ 01_basics/loop2.java
@@ -0,0 +1,9 @@
+public class loop2 {
+    public static void main(String[] args) {
+        int i = 12;
+        
+        do{
+            System.out.println("Apana College");
+        } while(i < 11);
+    }
+}
